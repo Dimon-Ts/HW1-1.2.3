d49bc4bb66a9b539c64fb50c2fe2887492eabaf8
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-09-12 09:24:19 +0300

    Переменные.Часть1. Домашнее задание - 1. Домашнее задание - 2. Домашнее задание - 3.

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("задача 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Задача 2
+        System.out.println("задача 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat +4;
+        System.out.println(cat);
+        paper = paper +4;
+        System.out.println(paper);
+
+        // Задача 3
+        System.out.println("задача 3");
+        dog -= 3.5;
+        System.out.println(dog);
+        cat -= 1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
+    }
+}
